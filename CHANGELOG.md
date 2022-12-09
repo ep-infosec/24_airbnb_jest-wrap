2b44fbe91c21c76e26327d2857d4b378bfc536e1
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-09 21:25:25 +0200

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,66 @@
+1.7.0 / 2020-05-12
+==================
+  * [New] adds support for Jest ^26
+
+1.6.0 / 2020-01-22
+==================
+  * [New] adds support for Jest ^25
+
+1.5.0 / 2019-02-11
+==================
+  * [New] adds support for Jest ^24
+
+1.4.0 / 2018-06-08
+==================
+  * [New] adds support for Jest ^23
+
+1.3.1 / 2018-05-01
+=================
+  * [Fix] ensure that skip works inside plugins, and plugins with no changes but the mode work
+  * [Deps] update `semver`
+  * [Dev Deps] update `eslint`, `istanbul-lib-coverage`, `nsp`, `tape`, `eslint-plugin-import`
+
+1.3.0 / 2018-01-04
+=================
+  * [New] adds support for Jest ^22
+  * [Deps] update `function.prototype.name`, `is-primitive`, `object-inspect`
+  * [Dev Deps] update `eslint`, `eslint-config-airbnb-base`, `eslint-plugin-import`, `nsp`, `rimraf`
+  * Move repo to airbnb
+
+1.2.0 / 2017-05-12
+=================
+  * [New] add `jest` `v21` support
+  * [Deps] update `function-bind`, `function.prototype.name`, `object-inspect`, `semver`
+  * [Dev Deps] update `eslint`, `@ljharb/eslint-config`, `istanbul-lib-coverage`, `nsp`, `tape`
+  * [Tests] make a matrix of jests
+  * [Tests] only test major node versions; include `v8`
+  * [Tests] use `nvm install-latest-npm` to ensure newer npms don’t break on older nodes
+
+1.1.0 / 2017-05-12
+=================
+  * [New] Add jest 20 support (#9)
+  * [Docs] Correct links/badges in the README (#8)
+  * [Tests] on `node` `v7.10`
+  * [Tests] Correct jest18/19 in package.json (#5)
+
+1.0.2 / 2017-04-11
+=================
+  * [Fix] Fix descriptions when using multiple wrappers (#4)
+  * [Fix] Stop reversing the afterEach hooks (#4)
+  * [Fix] Update global beforeAll/afterAll hooks (#4)
+  * [Fix] Remove .specify() (#4)
+  * [Deps] update `object-inspect`
+  * [Dev Deps] update `eslint`, `istanbul-lib-coverage`
+  * [Tests] up to `node` `v7.9`
+  * [Tests] Update core.js tests to work around bug in jest (#4)
+
+1.0.1 / 2017-03-16
+=================
+  * [Fix] avoid exponentially adding outer wrappers (#3)
+  * [Deps] lock `isarray` down to v1 only (v2 has a silly deprecation warning)
+  * [Dev Deps] update `eslint`, `nsp`, `jest`, `rimraf`
+  * [Tests] up to `node` `v7.7`, `v6.10`, `v4.8`
+
+1.0.0 / 2017-02-18
+=================
+  * Initial release.
---
+++ test/jest/core.js
@@ -0,0 +1,182 @@
+'use strict';
+
+var assert = require('assert');
+var jestVersion = require('jest/package.json').version;
+var semver = require('semver');
+var wrap = require('../../');
+
+describe('core JestWrapper semantics', function () {
+	describe('when there are no transformations', function () {
+		it('throws when there are no transformations', function () {
+			assert['throws'](function () { wrap().describe('foo', function () {}); }, RangeError);
+			assert['throws'](function () { wrap().it('foo', function () {}); }, RangeError);
+			assert['throws'](function () { wrap().test('foo', function () {}); }, RangeError);
+		});
+
+		(semver.satisfies(jestVersion, '< 27') ? it : it.skip)('describe does not throw when the mode is "skip"', function () {
+			assert.doesNotThrow(function () { wrap().skip().describe('foo', function () {}); });
+		});
+
+		(semver.satisfies(jestVersion, '< 21') ? it : it.skip)('it/test do not throw when the mode is "skip"', function () {
+			assert.doesNotThrow(function () { wrap().skip().it('foo', function () {}); });
+			assert.doesNotThrow(function () { wrap().skip().test('foo', function () {}); });
+		});
+	});
+
+	var withNothing = function withNothing() {
+		return { description: 'i am pointless' };
+	};
+
+	var testingCount = 0;
+	var withTesting = function withTesting() {
+		return this.extend('(with Testing)', {
+			beforeEach: function withTestingBeforeEach() {
+				testingCount += 1;
+			}
+		});
+	};
+
+	var withFancyNoop = function withFancyNoop() {
+		return this.extend('(withFancyNoop)', {});
+	};
+
+	var withSkip = function withSkip() {
+		return this.skip().extend('i am skipped', {});
+	};
+
+	wrap()
+	.use(withSkip)
+	.it('skips a test when the plugin skips it', function () {
+		throw new SyntaxError('this should never run');
+	});
+
+	wrap()
+	.use(withTesting)
+	.use(withFancyNoop)
+	.describe('with multiple plugins', function () {
+		it('calls the plugin\'s hooks an appropriate number of times', function () {
+			assert.equal(testingCount, 1);
+		});
+	});
+
+	describe('#use()', function () {
+		var flag = false;
+		var withDescriptor = function withDescriptor() {
+			return {
+				description: 'i am a descriptor',
+				beforeEach: function () { flag = true; },
+				afterEach: function () { flag = false; }
+			};
+		};
+
+		wrap().use(withDescriptor).describe('with a plugin that returns a descriptor', function () {
+			it('works', function () {
+				assert.equal(flag, true);
+			});
+		});
+
+		wrap().use(withNothing).it('works with a plugin that returns a descriptor with only a description', function () {
+			assert.equal(true, true); // oh yeah, TESTING
+		});
+	});
+
+	describe('#skip()', function () {
+		wrap().use(withNothing).skip().it('skipped an it!', function () {
+			assert.equal(true, false); // boom
+		});
+
+		wrap().use(withNothing).skip().describe('skipped a describe!', function () {
+			it('fails if not skipped', function () {
+				assert.equal(true, false); // boom
+			});
+		});
+
+		wrap().use(withNothing).skip().test('skipped a test!', function () {
+			it('fails if not skipped', function () {
+				assert.equal(true, false); // boom
+			});
+		});
+	});
+
+	/**
+	 * Temporarily replace describe so that we can assert on the passed params.
+	 */
+	var originalDescribe = global.describe;
+	var passedDescription;
+	global.describe = function (description) {
+		passedDescription = description;
+		global.describe = originalDescribe; // revert to mocha's describe.
+		return originalDescribe.apply(this, arguments);
+	};
+
+	wrap()
+	.use(withFancyNoop)
+	.use(withTesting)
+	.describe('wrapped descriptions', function () {
+		it('should have the proper description', function () {
+			assert.equal(passedDescription, 'wrapped: (withFancyNoop); (with Testing):');
+		});
+	});
+
+	var calls = [];
+	describe('ordering of before/afters with multiple plugins', function () {
+		var testsRun = 0;
+		wrap().extend('first', {
+			beforeAll: function () { calls.push('>:beforeAll'); },
+			beforeEach: function () { calls.push('>:beforeEach'); },
+			afterAll: function () { calls.push('>:afterAll'); },
+			afterEach: function () { calls.push('>:afterEach'); }
+		}).extend('second', {
+			beforeAll: function () { calls.push('>>:beforeAll'); },
+			beforeEach: function () { calls.push('>>:beforeEach'); },
+			afterAll: function () { calls.push('>>:afterAll'); },
+			afterEach: function () { calls.push('>>:afterEach'); }
+		}).describe('with method tracking', function () {
+			it('is one test', function () {
+				testsRun += 1;
+				calls.push('>>>:test');
+			});
+
+			it('is another test', function () {
+				testsRun += 1;
+				calls.push('>>>:test');
+			});
+		});
+
+		it('runs the hooks/tests in the correct order', function () {
+			if (testsRun !== 2) { throw new Error('This test cannot be run in isolation.'); }
+			var expectedCalls = [].concat(
+				'>:beforeAll',
+				'>>:beforeAll',
+
+				'>:beforeEach',
+				'>>:beforeEach',
+				'>>>:test',
+				semver.satisfies(jestVersion, '>= 27') ? [
+					'>:afterEach',
+					'>>:afterEach'
+				] : [
+					'>>:afterEach',
+					'>:afterEach'
+				],
+
+				'>:beforeEach',
+				'>>:beforeEach',
+				'>>>:test',
+				semver.satisfies(jestVersion, '>= 27') ? [
+					'>:afterEach',
+					'>>:afterEach',
+					'>:afterAll',
+					'>>:afterAll'
+				] : [
+					'>>:afterEach',
+					'>:afterEach',
+					'>>:afterAll',
+					'>:afterAll'
+				]
+			);
+
+			expect(calls).toEqual(expectedCalls);
+		});
+	});
+});
